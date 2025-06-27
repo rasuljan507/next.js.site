b56ebaf1726e46838279af17498a3f041a6ae3c3
rasuljan507 <ryusufaliyev@gmail.com>
2025-06-27 18:21:26 +0700

    Relax Eslint rules for new config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // ВОТ ЭТОТ БЛОК МЫ ДОБАВИЛИ
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // Меняем ошибку на предупреждение
+      "@typescript-eslint/no-explicit-any": "warn",  // Тоже меняем на предупреждение
+    },
+  },
 ];
 
 export default eslintConfig;
