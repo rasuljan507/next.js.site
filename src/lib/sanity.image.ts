31929d4fce62d141b6bb6dae61621bd0efd337d4
rasuljan507 <ryusufaliyev@gmail.com>
2025-06-28 17:56:29 +0700

    last update 3.0

--- src/lib/sanity.image.ts
+++ src/lib/sanity.image.ts
@@ -1,12 +1,9 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import { dataset, projectId } from './sanity.client'
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import { client } from './sanity.client'
 
-const imageBuilder = createImageUrlBuilder({
-  projectId: projectId || '',
-  dataset: dataset || '',
-})
+const imageBuilder = createImageUrlBuilder(client)
 
 export const urlFor = (source: SanityImageSource) => {
-  return imageBuilder.image(source)
+  return imageBuilder.image(source).auto('format').fit('max')
 }
