44dc7592007095af465760c0edd8e88c1701e39a
rasuljan507 <ryusufaliyev@gmail.com>
2025-06-29 12:07:50 +0700

    last update 14.0

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,33 +1,34 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { urlFor } from '@/lib/sanity.image';
 
-type ProjectCardProps = {
-  title: string;
-  imageUrl: string;
-  linkUrl: string;
-};
+interface ProjectCardProps {
+  project: {
+    _id: string;
+    title: string;
+    mainImage: any;
+  };
+}
 
-const ProjectCard = ({ title, imageUrl, linkUrl }: ProjectCardProps) => {
+const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
-    
-    <Link href={linkUrl} legacyBehavior={false}>
+    <Link href={`/portfolio/${project._id}`}>
       <div className="rounded-lg overflow-hidden group border border-gray-800 hover:border-[#F95738] transition-colors duration-300 cursor-pointer">
 
-        {/* Контейнер для картинки */}
         <div className="relative w-full h-56 bg-gray-900">
           <Image
-            src={imageUrl}
-            alt={`Скриншот проекта ${title}`}
-            fill 
-            style={{ objectFit: 'cover' }} 
+            src={urlFor(project.mainImage).url()}
+            alt={`Скриншот проекта ${project.title}`}
+            fill
+            style={{ objectFit: 'cover' }}
             className="group-hover:scale-105 transition-transform duration-300"
           />
         </div>
 
-        {/* Контейнер для текста */}
         <div className="p-4 bg-[#1F1F23]">
           <h3 className="text-lg font-semibold text-white truncate">
-            {title}
+            {/* Получаем заголовок из объекта project */}
+            {project.title}
           </h3>
         </div>
 
